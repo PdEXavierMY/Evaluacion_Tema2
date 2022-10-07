3ed892dbb13c1206c34f41f90b820cf9f2417d56
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-07 15:43:21 +0200

    añadido subclases extras

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,43 +1,38 @@
 class Vehiculo():
-
- 
-
     def __init__(self, color, ruedas):
-
         self.color = color
-
         self.ruedas = ruedas
 
  
 
     def __str__(self):
-
         return "color {}, {} ruedas".format( self.color, self.ruedas )
 
- 
-
- 
 
 class Coche(Vehiculo):
-
- 
-
     def __init__(self, color, ruedas, velocidad, cilindrada):
-
-        Vehiculo.__init__(self, color, ruedas)
-
+        super().__init__(color, ruedas)
         self.velocidad = velocidad
-
         self.cilindrada = cilindrada
 
- 
-
     def __str__(self):
-
         return Vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
 
- 
 
- 
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, modelo):
+        super().__init__(color, ruedas)
+        self.modelo = modelo
 
-c = Coche("azul", 4, 150, 1200)
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", Modelo {}".format(self.velocidad, self.cilindrada)
+
+
+class Tren(Vehiculo):
+    def __init__(self, color, ruedas, longuitud, antiguedad):
+        super().__init__(color, ruedas)
+        self.longuitud = longuitud
+        self.antiguedad = antiguedad
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} m de longuitud, {} años de antiguedad".format(self.velocidad, self.cilindrada)
