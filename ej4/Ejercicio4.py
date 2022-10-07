bcd6b8ad4afdeb4809c169c2dc0ab0472c57e09b
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-07 16:41:49 +0200

    .

---
+++ ej4/Ejercicio4.py
@@ -0,0 +1,36 @@
+#1) Código a evaluar:
+def c1(numero):
+    while True:
+        try:
+            numero
+        except ZeroDivisionError:
+            print("No se puede dividir entre 0")
+
+#2) Código a evaluar:
+def c2(lista):
+    try:
+        lista[10]
+    except IndexError:
+        print("La lista no tiene elemento en el índice introducido.")
+        
+#3) Código a evaluar:
+def c3(dicc):
+    try:
+        dicc["alemania"]
+    except KeyError:
+        print("No existe una key en el diccionario que se llame así.")
+
+#4) Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:
+def c4():
+    try:
+        resultado = "2" + 10
+    except TypeError:
+        print("Intentas operar elementos de tipos distintos.")
+
+n = 7/0
+l = [4, 7, 30, 23, 5]
+d = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+c1(n)
+c2(l)
+c3(d)
+c4()
